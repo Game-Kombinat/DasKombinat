0a07b5bd9a12d5e8176cd528d7230d9466fd6108
Chris <chris@gamekombinat.com>
2021-12-09 18:45:58 +0100

    Blueprint latent interpolator api
    Do interpolations wherever, whenever.
    Should have thought about that earlier.

---
+++ Source/DasKombinat/Private/InterpolatorHelper.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Game Kombinat
+
+
+#include "InterpolatorHelper.h"
+
+#include "Interpolator.h"
+
+UInterpolatorHelper* UInterpolatorHelper::Interpolate(UObject* worldContext, float duration) {
+    const auto interp = NewObject<UInterpolatorHelper>();
+
+    FAnim8Sample sample;
+    sample.BindUFunction(interp, "OnInterpolatorTick");
+
+    FAnim8Done done;
+    done.BindUFunction(interp, "OnFinished");
+    
+    FInterpolator::Anim8(worldContext->GetWorld(), duration, true, sample, done);
+    return interp;
+}
+
+void UInterpolatorHelper::OnFinished() {
+    if (done.IsBound()) {
+        done.Broadcast();
+    }
+}
+
+void UInterpolatorHelper::OnInterpolatorTick(const float t) {
+    if (tick.IsBound()) {
+        tick.Broadcast(t);
+    }
+}
---
+++ Source/DasKombinat/Public/InterpolatorHelper.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Game Kombinat
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "InterpolatorHelper.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FInterpolatorFinished);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FInterpolatorTick, float, alpha);
+/**
+ * 
+ */
+UCLASS(meta=(HideThen=true))
+class DASKOMBINAT_API UInterpolatorHelper : public UBlueprintAsyncActionBase  {
+    GENERATED_BODY()
+    
+    UPROPERTY(BlueprintAssignable)
+    FInterpolatorTick tick;
+
+    // It has to be the second property, otherwise the parameter of tick isn't shown.
+    UPROPERTY(BlueprintAssignable)
+    FInterpolatorFinished done;
+
+protected:
+
+    UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext="worldContext"))
+    static UInterpolatorHelper* Interpolate(UObject* worldContext, float duration);
+
+    UFUNCTION()
+    void OnFinished();
+
+    UFUNCTION()
+    void OnInterpolatorTick(float t);
+
+};
