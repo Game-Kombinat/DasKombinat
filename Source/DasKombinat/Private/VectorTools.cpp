2a59ab312b1f56d1b08da5c5211a1d7914745ba5
Chris <chris@gamekombinat.com>
2022-04-26 22:35:37 +0200

    Adding a DLH called VectorTools
    Basically just some code to detect if a directional vector is facing at a point in space and how much.
    
    I find this incredibly useful in general.

---
+++ Source/DasKombinat/Private/VectorTools.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VectorTools.h"
+
+float UVectorTools::CosineAngle(const FVector& a, const FVector& b) {
+    return a.GetSafeNormal().Dot((b - a).GetSafeNormal());
+}
+
+float UVectorTools::CosineAngleDirection(const FVector& a, const FVector& direction) {
+    return a.GetSafeNormal().Dot(direction.GetSafeNormal());
+}
+
+bool UVectorTools::IsFacing(const FVector& a, const FVector& b, const float precision) {
+    return CosineAngle(a, b) >= precision;
+    
+}
+
+bool UVectorTools::IsFacingAngle(const FVector& a, const FVector& b, const float angle) {
+    const float dot = CosineAngle(a, b);
+    const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
+    return vectorAngle <= angle;
+}
+
+bool UVectorTools::IsFacingAngleDirection(const FVector& a, const FVector& direction, float angle) {
+    const float dot = CosineAngleDirection(a, direction);
+    const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
+    return vectorAngle <= angle;
+}
---
+++ Source/DasKombinat/Public/VectorTools.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "VectorTools.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DASKOMBINAT_API UVectorTools : public UBlueprintFunctionLibrary {
+    GENERATED_BODY()
+public:
+    UFUNCTION(BlueprintCallable)
+    static float CosineAngle(const FVector& a, const FVector& b);
+    UFUNCTION(BlueprintCallable)
+    static float CosineAngleDirection(const FVector& a, const FVector& direction);
+    /**
+     * Is the directional vector a facing in the direction of positional vector b?
+     * This just takes a dot product of both vectors and checks the directionality.
+     * Precision parameter defines "how much" we need to face at vector b to be facing it.
+     */
+    UFUNCTION(BlueprintCallable)
+    static bool IsFacing(const FVector& a, const FVector& b, float precision = .55f);
+
+    /**
+     * Is the directional vector a facing in the direction of positional vector b?
+     * This takes a dot product of both vectors and checks the angle.
+     * Precision parameter defines the max angle for the direction a to be considered facing at vector b.
+     */
+    UFUNCTION(BlueprintCallable)
+    static bool IsFacingAngle(const FVector& a, const FVector& b, float angle = 45.f);
+
+    UFUNCTION(BlueprintCallable)
+    static bool IsFacingAngleDirection(const FVector& a, const FVector& direction, float angle = 45.f);
+
+};
