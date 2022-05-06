2b3c61351f433cd34f474229f3ae8126e17b5fa6
Chris <chris@gamekombinat.com>
2022-05-06 21:33:30 +0200

    The stupid, it knows no end
    I don't know. Obviously I must have been drunk or otherwise out of it.

--- Source/DasKombinat/Private/VectorTools.cpp
+++ Source/DasKombinat/Private/VectorTools.cpp
@@ -3,27 +3,24 @@
 
 #include "VectorTools.h"
 
-float UVectorTools::CosineAngle(const FVector& normal, const FVector& facingThis) {
-    return normal.GetSafeNormal().Dot((facingThis - normal).GetSafeNormal());
+float UVectorTools::CosineAngle(const FVector& normal, const FVector& direction) {
+    return FVector::DotProduct(normal, direction);
 }
 
-float UVectorTools::CosineAngleDirection(const FVector& normal, const FVector& direction) {
-    return normal.GetSafeNormal().Dot(direction.GetSafeNormal());
-}
 
-bool UVectorTools::IsFacing(const FVector& normal, const FVector& facingThis, const float precision) {
-    return CosineAngle(normal, facingThis) >= precision;
+bool UVectorTools::IsFacing(const FVector& normal, const FVector& fromHere, const FVector& facingThis, const float precision) {
+    return CosineAngle(normal, (facingThis - fromHere).GetSafeNormal()) >= precision;
     
 }
 
-bool UVectorTools::IsFacingAngle(const FVector& normal, const FVector& facingThis, const float angle) {
-    const float dot = CosineAngle(normal, facingThis);
+bool UVectorTools::IsFacingAngle(const FVector& normal, const FVector& fromHere, const FVector& facingThis, const float angle) {
+    const float dot = CosineAngle(normal, (facingThis - fromHere).GetSafeNormal());
     const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
     return vectorAngle <= angle;
 }
 
 bool UVectorTools::IsFacingAngleDirection(const FVector& normal, const FVector& direction, float angle) {
-    const float dot = CosineAngleDirection(normal, direction);
+    const float dot = CosineAngle(normal, direction);
     const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
     return vectorAngle <= angle;
 }
--- Source/DasKombinat/Public/VectorTools.h
+++ Source/DasKombinat/Public/VectorTools.h
@@ -15,15 +15,14 @@ class DASKOMBINAT_API UVectorTools : public UBlueprintFunctionLibrary {
 public:
     UFUNCTION(BlueprintCallable)
     static float CosineAngle(const FVector& normal, const FVector& facingThis);
-    UFUNCTION(BlueprintCallable)
-    static float CosineAngleDirection(const FVector& normal, const FVector& direction);
+
     /**
      * Is the directional vector a facing in the direction of positional vector b?
      * This just takes a dot product of both vectors and checks the directionality.
      * Precision parameter defines "how much" we need to face at vector b to be facing it.
      */
     UFUNCTION(BlueprintCallable)
-    static bool IsFacing(const FVector& normal, const FVector& facingThis, float precision = .55f);
+    static bool IsFacing(const FVector& normal, const FVector& fromHere, const FVector& facingThis, float precision = .55f);
 
     /**
      * Is the directional vector a facing in the direction of positional vector b?
@@ -31,7 +30,7 @@ public:
      * Precision parameter defines the max angle for the direction a to be considered facing at vector b.
      */
     UFUNCTION(BlueprintCallable)
-    static bool IsFacingAngle(const FVector& normal, const FVector& facingThis, float angle = 45.f);
+    static bool IsFacingAngle(const FVector& normal, const FVector& fromHere, const FVector& facingThis, float angle = 45.f);
 
     UFUNCTION(BlueprintCallable)
     static bool IsFacingAngleDirection(const FVector& normal, const FVector& direction, float angle = 45.f);
