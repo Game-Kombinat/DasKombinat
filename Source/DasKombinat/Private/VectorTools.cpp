28e5958d42c5995b6967fdac40e7b4bc93bb3e93
Chris Ksoll <chris@gamekombinat.com>
2023-05-12 21:46:55 +0200

    Add some extras to vector tools about actor heading

--- Source/DasKombinat/Private/VectorTools.cpp
+++ Source/DasKombinat/Private/VectorTools.cpp
@@ -3,28 +3,36 @@
 
 #include "VectorTools.h"
 
-float UVectorTools::Dot(const FVector& normal, const FVector& direction) {
-    return FVector::DotProduct(normal, direction);
+
+float UVectorTools::FindHeading(const FTransform& fromHere, const FVector& toHere) {
+    return FVector::DotProduct(fromHere.Rotator().Vector(), (toHere - fromHere.GetLocation()).GetSafeNormal());
 }
 
+float UVectorTools::FindHeading(const AActor* from, const AActor* to) {
+    return FindHeading(from->GetTransform(), to->GetActorLocation());
+}
+
+float UVectorTools::K2_FindHeadingWithActors(const AActor* from, const AActor* to) {
+    return FindHeading(from, to);
+}
 
 bool UVectorTools::IsFacing(const FVector& normal, const FVector& fromHere, const FVector& facingThis, const float precision) {
-    return Dot(normal, (facingThis - fromHere).GetSafeNormal()) >= precision;
+    return FVector::DotProduct(normal, (facingThis - fromHere).GetSafeNormal()) >= precision;
     
 }
 
 bool UVectorTools::IsFacingDirection(const FVector& direction, const FVector& targetDirection, float precision) {
-    return Dot(direction, targetDirection) >= precision;
+    return FVector::DotProduct(direction, targetDirection) >= precision;
 }
 
 bool UVectorTools::IsFacingWithinAngle(const FVector& normal, const FVector& fromHere, const FVector& facingThis, const float angle) {
-    const float dot = Dot(normal, (facingThis - fromHere).GetSafeNormal());
+    const float dot = FVector::DotProduct(normal, (facingThis - fromHere).GetSafeNormal());
     const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
     return vectorAngle <= angle;
 }
 
 bool UVectorTools::IsFacingDirectionWithinAngle(const FVector& normal, const FVector& direction, float angle) {
-    const float dot = Dot(normal, direction);
+    const float dot = FVector::DotProduct(normal, direction);
     const float vectorAngle = FMath::RadiansToDegrees(FMath::Acos(dot));
     return vectorAngle <= angle;
 }
--- Source/DasKombinat/Public/VectorTools.h
+++ Source/DasKombinat/Public/VectorTools.h
@@ -12,9 +12,21 @@
 UCLASS()
 class DASKOMBINAT_API UVectorTools : public UBlueprintFunctionLibrary {
     GENERATED_BODY()
+
 public:
+    /**
+     * How much is transform fromHere looking in the direction to location toHere?
+     */
     UFUNCTION(BlueprintCallable)
-    static float Dot(const FVector& normal, const FVector& facingThis);
+    static float FindHeading(const FTransform& fromHere, const FVector& toHere);
+
+    /**
+     * How much is actor from looking in the direction to actor to?
+     */
+    static float FindHeading(const AActor* from, const AActor* to);
+ 
+    UFUNCTION(BlueprintCallable, meta=(DisplayName="Find Heading between Actors"))
+    static float K2_FindHeadingWithActors(const AActor* from, const AActor* to);
 
     /**
      * Is the directional vector a facing in the direction of positional vector b?
@@ -22,7 +34,8 @@ public:
      * Precision parameter defines "how much" we need to face at vector b to be facing it.
      */
     UFUNCTION(BlueprintCallable)
-    static bool IsFacing(const FVector& normal, const FVector& fromHere, const FVector& facingThis, float precision = .55f);
+    static bool IsFacing(const FVector& normal, const FVector& fromHere, const FVector& facingThis,
+                         float precision = .55f);
 
     UFUNCTION(BlueprintCallable, BlueprintPure)
     static bool IsFacingDirection(const FVector& direction, const FVector& targetDirection, float precision = .55f);
@@ -33,7 +46,8 @@ public:
      * Precision parameter defines the max angle for the direction a to be considered facing at vector b.
      */
     UFUNCTION(BlueprintCallable)
-    static bool IsFacingWithinAngle(const FVector& normal, const FVector& fromHere, const FVector& facingThis, float angle = 45.f);
+    static bool IsFacingWithinAngle(const FVector& normal, const FVector& fromHere, const FVector& facingThis,
+                                    float angle = 45.f);
 
     UFUNCTION(BlueprintCallable)
     static bool IsFacingDirectionWithinAngle(const FVector& normal, const FVector& direction, float angle = 45.f);
